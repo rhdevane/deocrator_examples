2be04419004cc11e090d1514ea65ab779a47cb0d
manhattan <rhdevane@gmail.com>
2020-12-12 23:42:29 -0500

    Initial commit

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import decorators as decor
 
+
 def say_whee():
     print("Whee!")
+
 
 @decor.my_decorator
 def my_function():
     print('my function')
 
+
 @decor.not_during_the_night
 def say_whee():
     print("Whee!")
 
-# Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
 
+    print("git change")
+    # test
     my_function()
     say_whee()
